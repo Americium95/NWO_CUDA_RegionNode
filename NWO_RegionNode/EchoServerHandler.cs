034a87140b9fc9271d49dd19c59dc092cea3368b
Americium <sunk0525@naver.com>
2022-06-18 01:24:38 +0900

    2022_06_18

--- NWO_RegionNode/EchoServerHandler.cs
+++ NWO_RegionNode/EchoServerHandler.cs
@@ -6,30 +6,37 @@ using System.Text;
 
 public class EchoServerHandler : ChannelHandlerAdapter
 {
-    
+
     public override void ChannelRead(IChannelHandlerContext context, object message)
     {
         var buffer = message as IByteBuffer;
-        Console.WriteLine("수신:" + buffer.ToString(Encoding.UTF8));
-        
 
-        IByteBuffer buf = Unpooled.CopiedBuffer(Encoding.UTF8.GetBytes((buffer.ToString(Encoding.UTF8)).ToString()));
+        string rcv = buffer.ToString(Encoding.UTF8).Substring(2);
+        Console.WriteLine("수신:" + rcv);
+
+
+
 
         User Data;
 
-        if (!Program.userTable.TryGetValue(buffer.ToString(Encoding.UTF8)[5], out Data))
+        if (rcv.IndexOf("user") > -1)
         {
-            Program.userTable.Add(buffer.ToString(Encoding.UTF8)[5], new User(0, new Vector3(0, 0, 0), 0));
+            if (!Program.userTable.TryGetValue(rcv[rcv.IndexOf("user") + 4], out Data))
+            {
+                Program.userTable.Add(rcv[rcv.IndexOf("user") + 4], new User(0, new Vector3(0, 0, 0), 0));
 
-            Data = Program.userTable[buffer.ToString(Encoding.UTF8)[5]];
-        }
-        else
-        {
-            Data.position = new Vector3(0, 0, 0);
+                Data = Program.userTable[rcv[rcv.IndexOf("user") + 4]];
+            }
+            else
+            {
+                Data.position = new Vector3(0, 0, 0);
+            }
         }
         //context.WriteAsync("dtd");
 
-        context.WriteAsync(buffer);
+        IByteBuffer buf = Unpooled.CopiedBuffer(Encoding.UTF8.GetBytes("out:"+Program.userTable.Count));
+
+        context.WriteAsync(buf);
 
         //Console.WriteLine("송신:"+buf.ToString(Encoding.UTF8));
     }
