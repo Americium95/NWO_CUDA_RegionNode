f579e7b1c8c3474d42e1a64825a0d111da469e0d
Americium <sunk0525@naver.com>
2022-06-20 01:40:08 +0900

    sunk0525.iptime.org

--- NWO_RegionNode/EchoServerHandler.cs
+++ NWO_RegionNode/EchoServerHandler.cs
@@ -18,23 +18,45 @@ public class EchoServerHandler : ChannelHandlerAdapter
 
 
         User Data;
+        int userIndex = rcv.IndexOf("user");
+        string[] rcvData = rcv.Split(new char[] {',',':'});
 
-        if (rcv.IndexOf("user") > -1)
+
+        if (userIndex > -1)
         {
-            if (!Program.userTable.TryGetValue(rcv[rcv.IndexOf("user") + 4], out Data))
+            if (!Program.userTable.TryGetValue(int.Parse(rcv[userIndex + 4].ToString()), out Data))
             {
-                Program.userTable.Add(rcv[rcv.IndexOf("user") + 4], new User(0, new Vector3(0, 0, 0), 0));
+                Program.userTable.Add(int.Parse(rcv[userIndex + 4].ToString()), new User(0, new Vector3(0, 0, 0), 0));
 
-                Data = Program.userTable[rcv[rcv.IndexOf("user") + 4]];
+                Data = Program.userTable[int.Parse(rcv[userIndex + 4].ToString())];
             }
             else
             {
-                Data.position = new Vector3(0, 0, 0);
+                Data.position = new Vector3(int.Parse(rcvData[1]), int.Parse(rcvData[2]), int.Parse(rcvData[3]));
             }
         }
+
+        
+
+
         //context.WriteAsync("dtd");
 
-        IByteBuffer buf = Unpooled.CopiedBuffer(Encoding.UTF8.GetBytes("out:"+Program.userTable.Count));
+        StringBuilder std = new StringBuilder();
+
+        foreach (var userData in Program.userTable)
+        {
+            std.Append("{");
+            std.Append(userData.Key);
+            std.Append(",");
+            std.Append(userData.Value.position.X);
+            std.Append(",");
+            std.Append(userData.Value.position.Y);
+            std.Append(",");
+            std.Append(userData.Value.position.Z);
+            std.Append("}");
+        }
+
+        IByteBuffer buf = Unpooled.CopiedBuffer(Encoding.UTF8.GetBytes(std.ToString()));
 
         context.WriteAsync(buf);
 
